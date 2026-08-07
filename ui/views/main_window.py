012124a87e323e8a976e771a458ed4367c75ef92
Rony <ronybiswasinnovative@gmail.com>
2026-08-07 23:50:53 +0600

    Improved project structure

--- ui/views/main_window.py
+++ ui/views/main_window.py
@@ -1,16 +1,19 @@
 from qframelesswindow import FramelessWindow
 from app.config import WINDOW_HEIGHT, WINDOW_WIDTH
+from ui.widgets.window_title import WindowTitle
 
 
 class MainWindow(FramelessWindow):
     """
-    Main application window containing Editor, Left Panel, 
-    Right Panel, Down Panel etc.
+    Main application window containing TitleBar, Editor,
+    Left Panel, Right Panel, Down Panel etc.
     """
     def __init__(self, parent=None):
         """ Initialize the main window """
         super().__init__(parent=parent)
-        
+        self.window_title = WindowTitle(self)
+
+        self.setTitleBar(self.window_title)
         self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
         
         
