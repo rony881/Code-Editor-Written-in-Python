4b1ab9062d2c949e5b135b3f486db30e3bd03bd2
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-08 17:45:52 +0600

    Update window_title.py

--- ui/window_title.py
+++ ui/window_title.py
@@ -1,12 +1,15 @@
-from qframelesswindow import TitleBar
+from qframelesswindow import StandardTitleBar
 from ui.window_menubar import EditorMenuBar
-class WindowTitle(TitleBar):
+
+
+class WindowTitle(StandardTitleBar):
     """Custom title bar for the Application."""
 
     def __init__(self, parent):
         super().__init__(parent)
         self.setObjectName("window_title")
-        self.menu_bar = EditorMenuBar(window=parent, parent=self)
-        
-        self.raise_()
+        self.setTitle("PyCode")
+        self.menu_bar = EditorMenuBar(window=parent)
 
+        self.hBoxLayout.insertWidget(3, self.menu_bar) 
+        self.raise_()
