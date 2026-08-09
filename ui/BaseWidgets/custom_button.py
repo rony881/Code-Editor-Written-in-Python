2ee7de5ec6bcd21e99ce1bdb45d5a6cc533b667b
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-09 19:15:18 +0600

    Create custom_button.py

---
+++ ui/BaseWidgets/custom_button.py
@@ -0,0 +1,32 @@
+from PyQt6.QtWidgets import QPushButton
+
+
+class CustomButton(QPushButton):
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent)
+
+        self.setObjectName("custom_button")
+        self.setFixedSize(28, 28)
+        self.setText(text)
+        self.setStyleSheet(self._get_style())
+
+    def _get_style(self) -> str:
+        return """
+        QPushButton#custom_button {
+            background: #21262d;
+            color: #c9d1d9;
+            border: 1px solid #30363d;
+            border-radius: none;
+            padding: 0px;
+            font-size: 13px;
+        }
+
+        QPushButton#custom_button:hover {
+            background: #30363d;
+            border-color: #8b949e;
+        }
+
+        QPushButton#custom_button:pressed {
+            background: #1f6feb;
+        }
+        """
