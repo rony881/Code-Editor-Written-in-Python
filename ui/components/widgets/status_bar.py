2fc0a898f84ae05a8eebb13794c369c0bcfec544
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-09 19:15:13 +0600

    Create status_bar.py

---
+++ ui/components/widgets/status_bar.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QHBoxLayout
+
+from ui.BaseWidgets.custom_button import CustomButton
+from ui.BaseWidgets.widget_base import BaseWidget
+
+
+class ActivityBar(BaseWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.setObjectName("activity_bar")
+        self.setFixedHeight(30)
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.setContentsMargins(5,0,0,0)
+        self.h_layout.setSpacing(0)
+        self.add(self.h_layout)
+
+        explorer_btn = CustomButton("E")
+        git_btn = CustomButton("G")
+        agent_btn = CustomButton("A")
+        terminal_btn = CustomButton("T")
+
+        self.h_layout.addWidget(explorer_btn)
+        self.h_layout.addWidget(git_btn)
+        self.h_layout.addWidget(agent_btn)
+        self.h_layout.addWidget(terminal_btn)
+        self.h_layout.addStretch(1)
+        
