bebe7b9f8dcfd3b0e794a06ad72ba8bff95a5ccf
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-10 23:02:53 +0600

    Update left_panel.py

--- ui/components/panels/left_panel.py
+++ ui/components/panels/left_panel.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QStackedWidget
+from PyQt6.QtWidgets import QHBoxLayout, QStackedWidget
 
+from ui.BaseWidgets.custom_button import CustomButton
 from ui.BaseWidgets.widget_base import BaseWidget
 from ui.components.widgets.agent_panel import AgentPanel
 from ui.components.widgets.file_explorer import FileExplorer
@@ -21,20 +22,41 @@ class LeftPanel(BaseWidget):
         self.setMinimumWidth(220)
         self.setMaximumWidth(400)
 
-        self.file_explorer = FileExplorer()
-        self.git_panel = GitPanel()
-        self.agent_panel = AgentPanel()
-        self.terminal_panel = TerminalPanel()
-
         self.stack = QStackedWidget()
-
+        self.add(self.stack)
+        
+        self.file_explorer = FileExplorer()
         self.stack.addWidget(self.file_explorer)
+        
+        self.git_panel = GitPanel()
         self.stack.addWidget(self.git_panel)
+
+        self.agent_panel = AgentPanel()
         self.stack.addWidget(self.agent_panel)
+
+        self.terminal_panel = TerminalPanel()
         self.stack.addWidget(self.terminal_panel)
 
-        self.add(self.stack)
+        self.btn_area = QHBoxLayout()
+        self.add(self.btn_area)
 
+        explorer_btn = CustomButton("E")
+        explorer_btn.clicked.connect(lambda: self.showPanel("File Explorer"))
+        
+        git_btn = CustomButton("G")
+        git_btn.clicked.connect(lambda: self.showPanel("Git Panel"))
+        
+        agent_btn = CustomButton("A")
+        agent_btn.clicked.connect(lambda: self.showPanel("Agent Panel"))
+        
+        terminal_btn = CustomButton("T")
+        terminal_btn.clicked.connect(lambda: self.showPanel("Terminal Panel"))
+
+        self.btn_area.addWidget(explorer_btn)
+        self.btn_area.addWidget(git_btn)
+        self.btn_area.addWidget(agent_btn)
+        self.btn_area.addWidget(terminal_btn)
+        
     def showPanel(self, panel_name: str):
         index = Panels[panel_name]
         self.stack.setCurrentIndex(index)
