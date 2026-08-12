0767f7927faf93b6fe98e356b2bb7c81f0dc67da
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-12 23:04:40 +0600

    Add STYLE_SHEET_FILE(style.qss)

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 720
 WINDOW_LOGO = str(PROJECT_ROOT / "resources" / "icons" / "logo.png")
+
+# Style Sheet Dir
+STYLE_SHEET_FILE = str(PROJECT_ROOT / "ui" / "themes" / "style.qss")
