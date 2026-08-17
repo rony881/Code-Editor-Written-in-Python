8226f0c2dd4d3924d970fd9816296e42ac2ffb8f
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-17 23:40:46 +0600

    feat: add left panel buttons

--- ui/main_statusbar.py
+++ ui/main_statusbar.py
@@ -1,5 +1,7 @@
 
 from PyQt6.QtWidgets import QPushButton, QStatusBar
+from ui.BaseWidgets.custom_button import CustomButton
+from ui.BaseWidgets.widget_base import BaseWidget
 
 
 class StatusBar(QStatusBar):
@@ -7,4 +9,21 @@ class StatusBar(QStatusBar):
         super().__init__(parent)
         self.setFixedHeight(30)
 
+        self.left_panel_buttons = LeftPanelButtons(self)
+        self.addWidget(self.left_panel_buttons)
+
+class LeftPanelButtons(BaseWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+
+        self.explorer_btn = CustomButton("E")
+        self.add(self.explorer_btn)
+
+        self.git_btn = CustomButton("G")
+        self.add(self.git_btn)
+
+    def setExplorerBtnConn(self, func):
+        self.explorer_btn.clicked.connect(func)
         
+    def setGitBtnConn(self, func):
+        self.git_btn.clicked.connect(func)
