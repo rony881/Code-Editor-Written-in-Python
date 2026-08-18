4b05215df276acdce584bd9b5c8f6788a5a43134
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-18 22:44:30 +0600

    remove buttons

--- ui/components/panels/right_panel.py
+++ ui/components/panels/right_panel.py
@@ -1,6 +1,4 @@
-from PyQt6.QtWidgets import QHBoxLayout, QStackedWidget
-
-from ui.BaseWidgets.custom_button import CustomButton
+from PyQt6.QtWidgets import QStackedWidget
 from ui.BaseWidgets.widget_base import BaseWidget
 from ui.components.widgets.agent_panel import AgentPanel
 from ui.components.widgets.terminal_panel import TerminalPanel
@@ -28,18 +26,6 @@ class RightPanel(BaseWidget):
             "Agent Panel": 0,
             "Terminal Panel": 1,
         }
-
-        self.btn_area = QHBoxLayout()
-        self.add(self.btn_area)
-
-        agent_btn = CustomButton("A")
-        agent_btn.clicked.connect(lambda: self.showPanel("Agent Panel"))
-        
-        terminal_btn = CustomButton("T")
-        terminal_btn.clicked.connect(lambda: self.showPanel("Terminal Panel"))
-
-        self.btn_area.addWidget(agent_btn)
-        self.btn_area.addWidget(terminal_btn)
 
     def get_panel_index(self, panel_name: str) -> int:
         return self._panels[panel_name]
