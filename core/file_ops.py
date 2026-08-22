e5b45c595dd8e3cd0e65d626656795e2bc66cee2
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-22 23:08:35 +0600

    feat: set encoding to utf-8.

--- core/file_ops.py
+++ core/file_ops.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 
 def read_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
     file_name = Path(file_path).name
-    return (content, file_name)
+    return content, file_name
