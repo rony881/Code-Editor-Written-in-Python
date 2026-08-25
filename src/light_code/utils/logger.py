dace5ad5c55b5398fd6f2d36e80ef7a9f37fd51b
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-25 22:46:37 +0600

    feat: add logger setup.

--- src/light_code/utils/logger.py
+++ src/light_code/utils/logger.py
@@ -1,1 +1,16 @@
-# src/light_code/utils/logger.py
+# utils/logger.py 
+
+import logging
+
+logging.basicConfig(
+    format="%(levelname)s %(asctime)s: %(message)s (Line %(lineno)d [%(funcName)s])",
+    datefmt="%I:%M:%S %p",
+    level=logging.DEBUG,
+)
+
+
+def setup_logger(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+logger = setup_logger(__name__)
