ab51824bc6de3613190122c256e39de7be67cb81
Rony Biswas <ronybiswasinnovative@gmail.com>
2026-08-26 23:43:23 +0600

    feat: add write_file func and update read_file func

--- src/light_code/services/file_service.py
+++ src/light_code/services/file_service.py
@@ -9,10 +9,28 @@ def read_file(file_path):
     """
     Reads the content of a file and returns it along with the file name.
     """
-    logger.info(f"Reading file: {file_path}")
-    
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
+    try:
+        logger.info(f"Reading file: {file_path}")
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+    except Exception as e:
+        logger.error(f"Error reading file: {e}")
+        raise
+    else:
+        logger.info(f"Successfully read file: {file_path}")
+        file_name = Path(file_path).name
+        return content, file_name
 
-    file_name = Path(file_path).name
-    return content, file_name
+def write_file(file_path, content):
+    """
+    Writes content to a file.
+    """
+    try:
+        logger.info(f"Writing file: {file_path}")
+        with open(file_path, 'w', encoding='utf-8', newline="") as file:
+            file.write(content)
+    except Exception as e:
+        logger.error(f"Error writing file: {e}")
+        raise
+    else:
+        logger.info(f"Successfully wrote file: {file_path}")
